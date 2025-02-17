78f181d08aa70662916d76ecf99bcffafc7a0280
capybara120404 <d.l.borodin@outlook.com>
2025-02-17 14:59:15 +0300

    Initial files have been created

---
+++ laba2/target/debug/deps/laba2-490ec80cbd83f88c.d
@@ -0,0 +1,5 @@
+/home/daniil/dev-projects/rust_projects/laba2/laba2/target/debug/deps/liblaba2-490ec80cbd83f88c.rmeta: src/main.rs
+
+/home/daniil/dev-projects/rust_projects/laba2/laba2/target/debug/deps/laba2-490ec80cbd83f88c.d: src/main.rs
+
+src/main.rs:
