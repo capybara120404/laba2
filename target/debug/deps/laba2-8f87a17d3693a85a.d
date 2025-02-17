d6712aa9a9b53b4cc327eea2e45396ab48fbd49a
dshns <daria.n19@yandex.ru>
2025-02-17 15:16:36 +0300

    The Cargo configuration files and the main Rust file have been deleted, and a new file has been added. main.py

--- target/debug/deps/laba2-8f87a17d3693a85a.d
+++
@@ -1,5 +0,0 @@
-/home/daniil/dev-projects/rust_projects/laba2/laba2/target/debug/deps/liblaba2-8f87a17d3693a85a.rmeta: src/main.rs
-
-/home/daniil/dev-projects/rust_projects/laba2/laba2/target/debug/deps/laba2-8f87a17d3693a85a.d: src/main.rs
-
-src/main.rs:
