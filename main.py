0dd92d6e78d513eaf56489951d17f7911ccf2417
capybara120404 <d.l.borodin@outlook.com>
2025-02-24 14:48:21 +0300

    Created running code for the program to function

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello, World!!!")
+def main():
+    print("Добро пожаловать в калькулятор стоимости покраски автомобиля!")
+    detail, color = get_user_input()
+    cost = calculate_cost(detail, color)
+    print(f"Стоимость покраски детали '{detail}' в цвет '{color}': {cost:.2f} рублей")
+
+if name == "main":
+    main()
